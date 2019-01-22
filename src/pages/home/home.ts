f0d47e61e0a4b211a0b055da89f36c69f53869f7
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-22 13:56:54 +0100

    Adds placeholder content for articles index, displays them on homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  articles = [
+    { title: "Breaking News", lede: "This is fun!", category: "Sports" },
+    { title: "An Article", lede: "Cool!!", category: "Culture" }
+  ];
+  constructor(public navCtrl: NavController) {}
 }
