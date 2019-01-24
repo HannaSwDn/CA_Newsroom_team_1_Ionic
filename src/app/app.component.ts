f5fe10d1f5bad1a85c8e68794a441e52aeb13839
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-24 10:33:51 +0100

    Adds ionViewDidLoad function and fetching articles from api inside that function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
 
 import { HomePage } from '../pages/home/home';
 
@@ -17,16 +15,12 @@ export class MyApp {
 
   pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private http: HttpClient) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     this.pages = [
       { title: 'Home', component: HomePage }
     ];
-
-    this.http.get('https://cors-anywhere.herokuapp.com/https://craftynews.herokuapp.com/api/articles').subscribe((response) => {
-      
-    });
   }
 
   initializeApp() {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
-  articles = [
-    { title: "Breaking News", lede: "This is fun!", category: "Sports" },
-    { title: "An Article", lede: "Cool!!", category: "Culture" }
-  ];
-  constructor(public navCtrl: NavController) {}
+  articles: any;
+  constructor(public navCtrl: NavController, private http: HttpClient) {}
+
+  ionViewDidLoad = () => {
+    this.http.get('https://cors-anywhere.herokuapp.com/https://craftynews.herokuapp.com/api/articles').subscribe((response) => {
+      this.articles = response.articles;
+    });
+  }
 }
+
